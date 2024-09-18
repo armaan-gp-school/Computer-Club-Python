8b260717f468634b097f7e033894f5a9ec81773b
armaan-gp-school <hcps-guptaa5@henricostudents.org>
2024-09-18 19:42:58 +0000

    Finished grade averages

--- Grade_Calculator.py
+++ Grade_Calculator.py
@@ -26,10 +26,6 @@ classworks = {}
 quizzes = {}
 tests = {}
 
-classwork_percent = 25
-quiz_percent = 35
-test_percent = 40
-
 # Defines functions to add grades to storing system
 def take_classwork(assignment_name:str, grade:float):
     classworks[assignment_name] = grade
@@ -41,14 +37,42 @@ def take_test(assignment_name:str, grade:float):
     tests[assignment_name] = grade
 
 take_classwork("homework assignment", 100.0)
+take_classwork("hw", 95.0)
+take_classwork("homew", 0.0)
 print(classworks)
 print(list(classworks.values()))
 
-classwork_average = sum(list(classworks.values()))/len(list(classworks.values()))
-quiz_average = sum(list(quizzes.values()))/len(list(quizzes.values()))
-test_average = sum(list(tests.values()))/len(list(tests.values()))
+classwork_average = sum(list(classworks.values()))/len(list(classworks.values())) if len(list(classworks.values())) > 0 else 0
+quiz_average = sum(list(quizzes.values()))/len(list(quizzes.values())) if len(list(quizzes.values())) > 0 else 0
+test_average = sum(list(tests.values()))/len(list(tests.values())) if len(list(tests.values())) > 0 else 0
 
+# if classworks, quizzes, tests are present
 if len(classworks) > 0 and len(quizzes) > 0 and len(tests) > 0:
     grade_average = (classwork_average * 0.25) + (quiz_average * 0.35) + (test_average * 0.40)
+
+# if classworks, quizzes are present
 elif len(classworks) > 0 and len(quizzes) > 0 and len(tests) == 0:
-    grade_average = (classwork_average * 0.)
+    grade_average = (classwork_average * 0.58333) + (quiz_average * 0.41666)
+
+# if classworks, tests are present
+elif len(classworks) > 0 and len(quizzes) == 0 and len(tests) > 0:
+    grade_average = (classwork_average * 0.38333) + (test_average * 0.61666)
+
+# if classworks are present
+elif len(classworks) > 0 and len(quizzes) == 0 and len(tests) == 0:
+    grade_average = classwork_average
+
+# if quizzes, tests are present
+elif len(classworks) == 0 and len(quizzes) > 0 and len(tests) > 0:
+    grade_average = (quiz_average * 0.46666) + (test_average * 0.53333)
+
+# if quizzes are present
+elif len(classworks) == 0 and len(quizzes) > 0 and len(tests) == 0:
+    grade_average = quiz_average
+
+# if tests are present
+elif len(classworks) == 0 and len(quizzes) == 0 and len(tests) > 0:
+    grade_average = test_average
+
+print(classwork_average)
+print(grade_average)
