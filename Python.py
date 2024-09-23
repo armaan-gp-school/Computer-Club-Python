a0673e54d1b10cb081dc1e5a805cad470a7ef9dd
armaan-gp-school <hcps-guptaa5@henricostudents.org>
2024-09-23 15:06:33 +0000

    Computer Club conditonal statements intro

--- Python.py
+++ Python.py
@@ -1,1 +1,8 @@
-print("hi!!!")
+david_mean = input("Is David mean? T/F: ")
+
+if david_mean == "T":
+    print("David is mean.")
+elif david_mean == "F":
+    print("David isn't mean !!")
+else:
+    print("You didnt type T or F dumbahh jit")
